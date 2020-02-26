e9a66192690d2b110d026ee1cf5951de9590c0c0
masaaki-kamegai <gbmc.0301@gmail.com>
2020-02-26 12:24:57 +0900

    replaced for 自動更新機能

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -63,4 +63,35 @@ $(function(){
         alert("メッセージ送信に失敗しました");
       });
     });
-});
+
+    var reloadMessages = function() {
+      var last_message_id = $('.main_chat__messagelist__contents:last').data("message-id");
+      console.log(last_message_id)
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0) {
+          console.log('success');
+          var insertHTML = '';
+          $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+          });
+          $('.main_chat__messagelist').append(insertHTML);
+          $('.main_chat__messagelist').animate({ scrollTop: $('.main_chat__messagelist')[0].scrollHeight});
+        }
+      })
+      .fail(function() {
+        alert('error');
+      })
+    }
+
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+  setInterval(reloadMessages, 7000);
+}
+
+
+})
